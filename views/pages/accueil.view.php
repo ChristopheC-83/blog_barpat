fd033d8016dfd4a092bcfc60abeae8ef87e06ca9
ChristopheC83 <christophe.chiappetta@gmail.com>
2023-09-16 17:59:30 +0200

    fond dégradé ok

--- views/pages/accueil.view.php
+++ views/pages/accueil.view.php
@@ -1,5 +1,12 @@
-<div class="accueil">
+<div class="accueil container">
+    <h1>Le blog d'un developpeur web</h1>
 
-    <h1 class="tac">Accueil</h1>
+    <h2>Je compile sur ce petit morceau du web mes expériences.</h2>
+
+    <p>Ici, pas d'ingénieur ou de développeur qui sait tout sur tout.</p>
+    <p>Non, juste moi (et c'est déjà pas mal non !?!)</p>
+
+    <p>Je vais vous synthétiser les marches que j'estime essentielles dans votre parcours de développeur web. </p>
+
 
 </div>
