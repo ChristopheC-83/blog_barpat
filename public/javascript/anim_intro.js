ae9ddf3d5ec5fd56261ab2711f38b2b5bcf2ce21
ChristopheC83 <christophe.chiappetta@gmail.com>
2023-09-17 21:11:42 +0200

    on doit personaliser les js

--- public/javascript/anim_intro.js
+++ public/javascript/anim_intro.js
@@ -1,4 +1,3 @@
-
 const intro = document.getElementById("intro");
 const introCromagnon = document.getElementById("introCromagnon");
 const introFleche = document.getElementById("introFleche");
@@ -11,17 +10,28 @@ animIntro.from(introCromagnon, {
   x: -imageWidth,
   duration: 2,
   autoAlpha: 0,
+  ease: "power4.out",
+  delay: 0.5,
 });
-animIntro.from(introFleche, {
-  x: -imageWidth * 0.5,
-  duration: 1.25,
-  autoAlpha: 0,
-}, "-=0.5");
-animIntro.from(introDev, {
-  x: -imageWidth * 0.5,
-  duration: 1.25,
-  autoAlpha: 0,
-}, "-=0.5");
-
+animIntro.from(
+  introFleche,
+  {
+    x: -imageWidth * 0.75,
+    duration: 1.5,
+    autoAlpha: 0,
+    ease: "power4.out",
+  },
+  "-=1.5"
+);
+animIntro.from(
+  introDev,
+  {
+    x: -imageWidth * 0.75,
+    duration: 1.5,
+    autoAlpha: 0,
+    ease: "power4.out",
+  },
+  "-=1"
+);
 
 
--- views/pages/accueil.view.php
+++ views/pages/accueil.view.php
@@ -3,18 +3,27 @@
     <h1>Le blog d'un developpeur web</h1>
     <div class="intro">
 
-        <h2>Je compile sur ce petit morceau du web mes expériences.</h2>
+        <h2>Je partage sur ce petit morceau du web mes expériences.</h2>
 
         <p>Ici, pas d'ingénieur ou de développeur qui sait tout sur tout.</p>
-        <p>Non, juste moi (et c'est déjà pas mal non !?!)</p>
+        <p>Non, juste moi (et c'est déjà pas mal non !?!) et mon pragmatisme !</p>
+        <p>Je vais vous compiler les marches que j'estime essentielles dans votre parcours de développeur web, surtout si vous partez du niveau de Néandertal  (ou pas loin).</p>
 
-        <p>Je vais vous synthétiser les marches que j'estime essentielles dans votre parcours de développeur web. </p>
     </div>
+
     <div class="evolution">
         <img src="<?= imgFolder ?>accueil/introCromagnon.jpg" alt="homme préhistorique" id="introCromagnon">
-        <i class="fa-regular fa-circle-right" id="introFleche"></i>
+        <i class="fa-regular fa-circle-right " id="introFleche"></i>
         <img src="<?= imgFolder ?>accueil/introDev.jpg" alt="écran de développeur" id="introDev">
     </div>
+
+    <div class="sections">
+
+
+
+    </div>
+
+
 
 
 </div>
