1a25bd5e7314d9892c9e8fe9a4b4e0addf2578b4
ChristopheC83 <christophe.chiappetta@gmail.com>
2023-09-18 18:52:45 +0200

    accueil pourri... on reprend tout !

--- views/pages/accueil.view.php
+++ views/pages/accueil.view.php
@@ -5,13 +5,14 @@
 
         <h2>Je partage sur ce petit morceau du web mes expériences.</h2>
 
-        <p>Ici, pas d'ingénieur ou de développeur qui sait tout sur tout.</p>
-        <p>Non, juste moi (et c'est déjà pas mal non !?!) et mon pragmatisme !</p>
-        <p>Je vous compile les étapes que j'estime essentielles dans votre parcours de développeur web.</p> <p>Passons ensemble du niveau de Néandertal (ou pas loin) à celui de dev.</p>
-        <h3>Important</h3>
-        <p>Prendre du plaisir !</p>
-        <p>Toujours faire des test / mini-projets perso</p>
-        <p>Prendre du plaisir !!!</p>
+        <p>Ici, pas d'ingénieur ou de développeur qui sait tout sur tout ! 🤯 </p>
+        <p>Non, juste moi (et c'est déjà pas mal non !?!) et mon pragmatisme 😉</p>
+        <p>Je vous compile les étapes que j'estime essentielles dans votre parcours de développeur web. 📶</p> <p>Passons ensemble du niveau de Néandertal (ou pas loin) à celui de dev. 🦣->🐘</p>
+        <h3>⚠️ Important ⚠️</h3>
+        <p>Prendre du plaisir ! ❤️</p>
+        <p>Toujours faire des test / mini-projets perso 🪒</p>
+        <p>Prendre du plaisir !!! 🥰</p>
+        <p>Etre fier(e) de chaque création ! 🤩</p>
 
     </div>
 
