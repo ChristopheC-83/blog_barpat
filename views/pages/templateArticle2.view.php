3f0e72be44aaa89adcc055c3984fbcc5b5db38bf
ChristopheC83 <christophe.chiappetta@gmail.com>
2023-10-04 13:25:10 +0200

    template2 ok

--- views/pages/templateArticle2.view.php
+++ views/pages/templateArticle2.view.php
@@ -6,15 +6,16 @@ $textes
  -->
 
 
-<article class="container templateArticle1">
-
+<article class="container templateArticle2">
 
     <h1 class="titre_article"><?= $textes[0]['titre'] ?></h1>
 
     <section class="part part1">
         <p><?= $textes[0]['texte'] ?></p>
         <?php if (isset($images[0]['url_img']) && $images[0]['url_img'] !== '') : ?>
-            <img src="<?= imgFolder ?><?= $infosArticle['theme'] ?>/<?= $images[0]['url_img'] ?>" alt="<?= $images[0]['alt_img'] ?>">
+            <a href="<?= isset($images[0]['lien']) ? $images[0]['lien'] : '' ?>">
+                <img src="<?= imgFolder ?><?= $infosArticle['theme'] ?>/<?= $images[0]['url_img'] ?>" alt="<?= $images[0]['alt_img'] ?>">
+            </a>
         <?php endif ?>
 
     </section>
@@ -24,9 +25,6 @@ $textes
     <?php endif ?>
 
     <section class="part part2">
-        <?php if (isset($images[1]['url_img']) && $images[1]['url_img'] !== '') : ?>
-            <img src="<?= imgFolder ?><?= $infosArticle['theme'] ?>/<?= $images[1]['url_img'] ?>" alt="<?= $images[1]['alt_img'] ?>">
-        <?php endif ?>
         <p><?= $textes[1]['texte'] ?></p>
     </section>
 
---
+++ views/pages/templateArticle3.view.php
@@ -0,0 +1,35 @@
+<!-- 
+$themes
+$infosArticle
+$images
+$textes
+ -->
+
+
+ <article class="container templateArticle2">
+
+
+<h1 class="titre_article"><?= $textes[0]['titre'] ?></h1>
+
+<section class="part part1">
+    <p><?= $textes[0]['texte'] ?></p>
+    <?php if (isset($images[0]['url_img']) && $images[0]['url_img'] !== '') : ?>
+      
+
+            <img src="<?= imgFolder ?><?= $infosArticle['theme'] ?>/<?= $images[0]['url_img'] ?>" alt="<?= $images[0]['alt_img'] ?>">
+       
+
+    <?php endif ?>
+
+</section>
+
+<?php if (isset($textes[1]['titre']) && $textes[1]['titre'] !== '') : ?>
+    <h2 class="titre_article"><?= $textes[1]['titre'] ?></h2>
+<?php endif ?>
+
+<section class="part part2">
+    <p><?= $textes[1]['texte'] ?></p>
+</section>
+
+
+</article>
